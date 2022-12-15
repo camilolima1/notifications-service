3bdfaeb0a18e42631fa09a597a89ec19539430d4
camilolima1 <caamilolima@gmail.com>
2022-12-15 19:56:29 -0300

    Add paths configurations

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,4 +1,4 @@
-import { InMemoryNotificationsRepository } from "../../../test/repositories/in-memory.notificatios-repository";
+import { InMemoryNotificationsRepository } from "@test/repositories/in-memory.notificatios-repository";
 import { SendNotification } from "./send-notification";
 
 describe('Send notification', () => {
