0c071ab25769f4fb9894cd1597d05ab0b518b968
camilolima1 <caamilolima@gmail.com>
2022-12-15 22:01:19 -0300

    Add notification mapper

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -17,6 +17,13 @@ export class NotificationsController {
       category,
     });
 
-    return { notification };
+    return { 
+      notification: {
+        id: notification.id,
+        content: notification.content.value,
+        category: notification.category,
+        recipientId: notification.recipientId,
+      }
+    };
   }
 }
