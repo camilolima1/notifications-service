a3dcf763405a19f75e7130a8570b44252fee8c2b
camilolima1 <caamilolima@gmail.com>
2022-12-16 23:38:33 -0300

    Add cancel notification use case

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -2,19 +2,20 @@ import { randomUUID } from "crypto";
 import { Replace } from "src/helpers/Replace";
 import { Content } from "./content";
 
-export interface NotificatioonProps {
+export interface NotificationProps {
     recipientId: string;
     content: Content;
     category: string;
     readAt?: Date | null;
+    canceledAt?: Date | null;
     createdAt: Date;
 }
 
 export class Notification {
     private _id: string;
-    private props: NotificatioonProps;
+    private props: NotificationProps;
 
-    constructor(props: Replace<NotificatioonProps, { createdAt?: Date}>){
+    constructor(props: Replace<NotificationProps, { createdAt?: Date}>){
         this._id = randomUUID();
         this.props = {
             ...props,
@@ -56,6 +57,14 @@ export class Notification {
 
     public get readAt(): Date | null | undefined {
         return this.props.readAt;
+    }
+
+    public cancel() {
+        this.props.canceledAt = new Date();
+    }
+
+    public get canceledAt(): Date | null | undefined {
+        return this.props.canceledAt;
     }
 
     public get createdAt(): Date {
